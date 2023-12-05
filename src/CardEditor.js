b19f0a0d3adc045166f7e8de7ae2a70777cdaf93
Jayden <jayden.lee0908@gmail.com>
2023-12-04 23:54:26 -0600

    clean 10

--- src/CardEditor.js
+++ src/CardEditor.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 class CardEditor extends React.Component {
     render(){
+        const cards = this.props.cards.map(card =>{
+            return (
+                <tr>
+                    <td>card front</td>
+                    <td>card back</td>
+                    <td>
+                        <button>Delete card</button>
+                    </td>
+                </tr>
+            )
+        })
         return(
             <div>
                 <h2>Card Editor</h2>
