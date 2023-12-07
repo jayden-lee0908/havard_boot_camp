244eee9214c5d4b65e8880dbd1d65b476207cbb7
Jayden <jayden.lee0908@gmail.com>
2023-12-06 18:33:28 -0600

    finished week2

--- src/CardEditor.js
+++ src/CardEditor.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import './CardEditor.css'
 
 class CardEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { front:'', back:'' }
+    }
+
+    addCard = () => {
+        if(!this.state.front.trim() || !this.state.back.trim()){
+            alert('Cannot add empty card');
+            return;
+        }
+
+        this.props.addCard(this.state);
+        this.setState({ front: '', back: '' });
+    };
+
+    deleteCard = index => this.props.deleteCard(index);
+
+    handleChange = event => this.setState({ [event.target.name]: event.target.value });
+
     render(){
-        const cards = this.props.cards.map(card =>{
+        const cards = this.props.cards.map((card, index) =>{
             return (
-                <tr>
-                    <td>card front</td>
-                    <td>card back</td>
+                <tr key={index}>
+                    <td>{card.front}</td>
+                    <td>{card.back}</td>
                     <td>
-                        <button>Delete card</button>
+                        <button 
+                        disabled={this.props.cards.length === 1}
+                        onClick={() => this.deleteCard(index)}>
+                            Delete card
+                        </button>
                     </td>
                 </tr>
-            )
-        })
+            );
+        });
+
         return(
             <div>
                 <h2>Card Editor</h2>
@@ -24,7 +49,25 @@ class CardEditor extends React.Component {
                             <th>Delete</th>
                         </tr>
                     </thead>
+                    <tbody>{cards}</tbody>
                 </table>
+                <br/>
+                <input 
+                name='front'
+                onChange={this.handleChange}
+                placeholder='Front of card' 
+                value={this.state.front}
+                />
+
+                <input 
+                name='back'
+                onChange={this.handleChange}
+                placeholder='Back of card'
+                value={this.state.back}
+                />
+                <button onClick={this.addCard}>Add card</button>
+                <hr />
+                <button onClick={this.props.switchMode}>Go to card viewer</button>
             </div>
         );
     }
---
+++ src/CardViewer.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './CardViewer.css'
+
+class CardViewer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            currentIndex: 0,
+            displayFront: true,
+        };
+    }
+
+    nextCard = () => {
+        if(this.state.currentIndex < this.props.cards.length - 1) {
+            this.setState({
+                currentIndex: this.state.currentIndex + 1,
+                displayFront: true,
+            });
+        }
+    };
+
+    prevCard = () => {
+        if (this.state.currentIndex > 0) {
+            this.setState({
+                currentIndex: this.state.currentIndex - 1,
+                displayFront: true,
+            });
+        }
+    };
+
+    flipCard = () => this.setState(
+        { displayFront: !this.state.displayFront }
+    );
+
+    render(){
+        const card = this.props.cards[this.state.currentIndex][
+            this.state.displayFront ? 'front' : 'back'
+        ];
+
+        return (
+            <div>
+                <h2>Card Viewer</h2>
+                Card {this.state.currentIndex+1} out of {this.props.cards.length}.
+                <div className='card' onClick={this.flipCard}>
+                    {card}
+                </div>
+                <br />
+                <button disabled={this.state.currentIndex===0} onClick={this.prevCard}>
+                    Prev card
+                </button>
+                <button disabled={this.state.currentIndex === this.props.cards.length-1} onClick={this.nextCard}>
+                    Next card
+                </button>
+                <hr />
+                <button onClick={this.props.switchMode}>Go to card editor</button>
+            </div>
+        );
+    }
+}
+
+export default CardViewer;
